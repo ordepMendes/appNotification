e243279ee6dccef8eb14695aec1495b0a4694cf4
ordepMendes <pedrohh0012@hotmail.com>
2024-04-05 17:42:33 -0300

    feat: First commit - teste de notifcação

--- App.tsx
+++ App.tsx
@@ -1,10 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, Button, StyleSheet, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 export default function App() {
+  const handleNotification = async () => {
+    const statusNotification = await Notifications.getPermissionsAsync();
+
+    if (statusNotification.status !== "granted") {
+      Alert.alert("Alerta", "Você não permitiu as notificações");
+      return;
+    }
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Notificação 1',
+        body: 'Primeira notificação'
+      },
+      trigger: {
+        seconds: 5,
+      }
+    })
+    
+    
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="chamar notificação" onPress={handleNotification} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +42,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
